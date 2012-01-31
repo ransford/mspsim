800240c8fc24e037739e7c48115e25fb458b8282
Ben Ransford <ransford@cs.umass.edu>
2012-01-31 09:40:55 -0500

    simple run+capacitor scripts

---
+++ runsimple.sh
@@ -0,0 +1,5 @@
+FIRMWARE="$1"
+test -n "$FIRMWARE" || exit 1
+EXTRAFLAGS="$*"
+
+java ${DEBUGOPTS} -classpath "mspsim.jar:lib/jcommon-1.0.14.jar:lib/jfreechart-1.0.11.jar:lib/jipv6.jar" se.sics.mspsim.platform.wisp.WispNode ${FIRMWARE} -nogui -autorun=scripts/simple.sc -exitwhendone ${EXTRAFLAGS}
