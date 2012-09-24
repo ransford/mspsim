c6ee0b66906743a5109c96f070660eb22392cfb3
Ben Ransford <ransford@cs.umass.edu>
2012-09-24 13:15:55 -0400

    simplify run scripts
    so it's clear that they differ by very little

--- runnocap.sh
+++ runnocap.sh
@@ -1,5 +1,3 @@
-FIRMWARE="$1"
-test -n "$FIRMWARE" || exit 1
-EXTRAFLAGS="$*"
+#!/bin/sh
 
-java ${DEBUGOPTS} -classpath "mspsim.jar:lib/jcommon-1.0.14.jar:lib/jfreechart-1.0.11.jar:lib/jipv6.jar" se.sics.mspsim.platform.crfid.MooNode ${FIRMWARE} -nogui -autorun=scripts/nocap.sc.tmpl -exitwhendone ${EXTRAFLAGS}
+exec ./run.sh -s scripts/nocap.sc.tmpl "$*"
--- runtrace.sh
+++ runtrace.sh
@@ -1,6 +1,3 @@
-FIRMWARE="$1"
-TRACE="$2"
-test -n "$FIRMWARE" || exit 1
-EXTRAFLAGS="$*"
+#!/bin/sh
 
-java ${DEBUGOPTS} -classpath "mspsim.jar:lib/jcommon-1.0.14.jar:lib/jfreechart-1.0.11.jar:lib/jipv6.jar" se.sics.mspsim.platform.crfid.MooNode ${FIRMWARE} -nogui -voltagetrace=${TRACE} -autorun=scripts/wisp.sc -exitwhendone ${EXTRAFLAGS}
+exec ./run.sh -t "$TRACE" -s scripts/moo.sc "$*"
