9a1efcf7059129018d0e6fad7632162f9cfc8502
Ben Ransford <ransford@cs.umass.edu>
2012-10-18 12:16:50 -0400

    run netbeans-generated mspsim.jar

--- run.sh
+++ run.sh
@@ -17,7 +17,7 @@ test -n "$FIRMWARE" || exit 1
 EXTRAFLAGS="$EXTRAFLAGS $*"
 
 java ${DEBUGOPTS} \
-	-classpath "mspsim.jar:lib/jcommon-1.0.14.jar:lib/jfreechart-1.0.11.jar:lib/jipv6.jar" \
+	-classpath "dist/mspsim.jar:lib/jcommon-1.0.14.jar:lib/jfreechart-1.0.11.jar:lib/jipv6.jar" \
 	se.sics.mspsim.platform.crfid.MooNode \
 	-nogui \
 	-exitwhendone \
